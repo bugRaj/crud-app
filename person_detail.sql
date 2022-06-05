3a121602ba4eeffa29e92487b07dd77b149e5ecb
Raj kr. soni <72248764+bugRaj@users.noreply.github.com>
2022-06-05 19:22:16 +0530

    Update person_detail.sql

--- person_detail.sql
+++ person_detail.sql
@@ -1,15 +1,15 @@
 
-CREATE TABLE `person_detail` (
+CREATE TABLE `student_person_detail` (
   `id` int(11) NOT NULL,
   `firstname` varchar(100) NOT NULL,
   `lastname` varchar(100) NOT NULL,
-  `email` varchar(150) NOT NULL,
+  `subject` varchar(150) NOT NULL,
   `phone` varchar(50) NOT NULL,
   `address` varchar(255) NOT NULL,
-  `postal_code` varchar(20) NOT NULL
+  `Age` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE `person_detail`
+ALTER TABLE `student_person_detail`
   ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `person_detail`
